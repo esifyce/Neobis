c4cc3bc84342a26c894ed4c7b9e02d192846f467
primarydose <49156359+primarydose@users.noreply.github.com>
2022-01-17 19:46:15 +0600

    Update Contents.swift

--- 2ndSection/Remove All The Marked Elements of a List.playground/Contents.swift
+++ 2ndSection/Remove All The Marked Elements of a List.playground/Contents.swift
@@ -2,19 +2,19 @@ import Foundation
 
 // MARK: - Define a method/function that removes from a given array of integers all the values contained in a second array.
 
-class Array {
+class Arrays {
     // func received two array and return array without duplicate in both
     func remove(firstArr: [Int], secondArr: [Int]) -> [Int] {
         // use filter which use iteration by first array in search no contain number in second array
-        let arrayResult = firstArr.filter { !secondArr.contains($0)
-        }
+        let arrayResult = firstArr.filter { !secondArr.contains($0) }
+        
         // total result in [Int] format
         return arrayResult
     }
 }
 
 // implement class
-var totalArr = Array()
+var totalArr = Arrays()
 
 // show results on console
 print(totalArr.remove(firstArr: [1, 1, 2 ,3 ,1 ,2 ,3 ,4],
