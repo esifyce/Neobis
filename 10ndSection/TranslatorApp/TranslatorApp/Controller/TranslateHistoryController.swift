cbcd48b7305230eb5cb434150655f959385e3f9a
esifyce <49156359+esifyce@users.noreply.github.com>
2022-03-19 00:19:28 +0600

    Update TranslateHistoryController.swift

--- 10ndSection/TranslatorApp/TranslatorApp/Controller/TranslateHistoryController.swift
+++ 10ndSection/TranslatorApp/TranslatorApp/Controller/TranslateHistoryController.swift
@@ -6,31 +6,67 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TranslateHistoryController: UITableViewController {
     
     // MARK: - Properties
-    var translators = Translator.translatorList
+    var translators = Translator.wordsList
+    private var realm: Realm?
+    private var words: Results<Translator>!
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        do {
+            self.realm = try Realm()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
+        words = realm?.objects(Translator.self)
+        
+        getData(translators)
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
         title = "iTranslator"
         tableView.register(TranslateHistoryCell.self,
                            forCellReuseIdentifier: TranslateHistoryCell.identifier)
+        
         view.backgroundColor = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
 
         tableView.rowHeight = 150
         tableView.separatorStyle = .none
+    }
+    
+    private func showError(_ error: Error?) {
+        if let error = error {
+            print("DEBUG: \(String(describing: error.localizedDescription))")
+            return
+        }
+    }
+    
+    private func getData(_ data: [Translator]) {
+        
+        realm?.beginWrite()
+        realm?.add((data.compactMap { $0 }))
+        
+        try! realm?.commitWrite()
+        
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return translators.count
+        return words.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -40,9 +76,9 @@ class TranslateHistoryController: UITableViewController {
             return UITableViewCell()
         }
         
-        cell.configure(input: translators[indexPath.row].inputWord,
-                       output: translators[indexPath.row].outputWord,
-                       languages: translators[indexPath.row].checkLanguage)
+        cell.configure(input: words[indexPath.row].inputWord,
+                       output: words[indexPath.row].outputWord,
+                       languages: words[indexPath.row].checkLanguage)
         cell.backgroundColor = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
         return cell
     }
@@ -51,3 +87,4 @@ class TranslateHistoryController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+
